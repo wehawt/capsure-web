acc3b0be7de06ffed3cdd067a69e24b06fe28fe5
wehawt <padriquejoshua@yahoo.com>
2021-11-27 13:13:29 +0000

    crud api with docker

--- capsure-app.sh
+++ capsure-app.sh
@@ -1,10 +1,11 @@
 #!bash .//bin/bash
 
+
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
-cp capsure-web.py tempdir/.
+cp capsure_web.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
@@ -13,11 +14,12 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  capsure-web.py /home/myapp/" >> tempdir/Dockerfile
-
+echo "COPY  capsure_web.py /home/myapp/" >> tempdir/Dockerfile
+echo "RUN pip install flask_sqlalchemy" >> tempdir/Dockerfile
+echo "RUN pip install flask_marshmallow" >> tempdir/Dockerfile
 echo "EXPOSE 8080" >> tempdir/Dockerfile
 
-echo "CMD python3 /home/myapp/capsure-web.py" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/capsure_web.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t capsure_app .
